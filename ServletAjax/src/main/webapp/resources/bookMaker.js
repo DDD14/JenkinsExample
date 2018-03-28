05d3ec4842ba2417a859796016e9289df4c1e601
DDD14 <davoldster@yahoo.com>
2018-03-28 14:06:09 -0400

    ServletAjax

---
+++ ServletAjax/src/main/webapp/resources/bookMaker.js
@@ -0,0 +1,84 @@
+function getBook(){
+	
+	console.log("inside get Book");
+	
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		console.log("ready state: " + xhr.readyState);
+		console.log("status: " + xhr.status);
+		if(xhr.readyState === 4 && xhr.status === 200){
+			var book = JSON.parse(xhr.responseText);
+			document.getElementById("stuff").innerHTML = book.title;
+			console.log(book);
+			
+		}
+		
+	}
+	
+	xhr.open("GET", "book", true);
+	xhr.send();
+	
+}
+
+function createBook(){
+	
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		
+		if(xhr.readyState === 4 && xhr.status === 200){
+			
+			document.getElementById("stuff").innerHTML = "book posted!!!";
+			
+		}
+		
+	}
+	
+	xhr.open("POST", "book", true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send("book="+JSON.stringify({title:"The Hungry Caterpiller 2", desc:"even better than the last", isbn:223344556, author:"Howard", pageCount: 5}));
+	
+}
+
+window.onload = function(){
+	
+	document.getElementById("getBook").addEventListener("click", getBook, false);
+	document.getElementById("postBook").addEventListener("click", createBook, false);
+	
+}
+
+
+
+/*
+function getBook(){
+	
+	console.log("inside getBook")
+	
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		
+		if(xhr.readystate == 4 && xhr.status == 200){
+			console.log("ready state: " + xhr.readystate);
+			console.log("status: " + xhr.status)
+			var book = JSON.parse(xhr.responsetext);
+			document.getElementById("stuff").innerHTML = book.title;
+			console.log(book);
+		}	
+	}
+	xhr.open("GET", "book", true)
+	xhr.send();
+}
+
+function createBook(){
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		
+		if(xhr.readystate == 4 && xhr.status == 200){
+			document.getElementById("stuff").innerHTML = "Book Posted!";
+		}
+	}
+	xhr.send("book="+JSON.Stringify({"Green Eggs and Ham", "another book", 123123123, "Dr.Seuss", 30}))
+}
+	window.onload = function(){
+	document.getElementById("getBook").addEventListener("click", getBook, false);
+	document.getElementById("createBook").addEventListener("click", createBook, false);
+}*/
